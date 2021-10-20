e76df8d522a4191dbafb4252915914626ff5eb4d
Kevin Gorham <kgorham@finetuneus.com>
2021-10-20 12:53:59 -0700

    added initial files

---
+++ queue.js
@@ -0,0 +1,12 @@
+// function addToQueue(runTask) {
+//   // addToQueue runs when the 'Queue it up!' button is clicked.
+
+//   // ...
+
+//   // Call runTask when the task is ready to start.
+//   runTask(function() {
+//     // The function passed to runTask will run when the task is done.
+
+//     // ...
+//   });
+// }
